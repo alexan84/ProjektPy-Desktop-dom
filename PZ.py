dc3bbeda1f859fe548b35f9e7e6cb1724fe92516
alexan84 <ivanovivandex@gmail.com>
2024-04-07 18:27:16 +0500

    1

--- PZ.py
+++ PZ.py
@@ -1,20 +1,72 @@
+# Dz 33
+
+
+
+import json
+from random import choice
+
+
+def gen_person():
+    name = ''
+    tel = ''
+
+    latter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'e']
+    num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+    while len(name) != 7:
+        name += choice(latter)
+
+    while len(tel) != 10:
+        tel += choice(num)
+
+    person = {
+        'name': name,
+        'tel': tel,
+    }
+    return person
+
+
+def get_key():
+    keys = ''
+    data = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+    while len(keys) != 10:
+        keys += choice(data)
+
+    return keys
+
+
+def write_json(person_dict):
+    try:
+        data = json.load(open('homework.json'))
+    except FileNotFoundError:
+        data = {}
+
+    data.update(person_dict)
+    with open('homework.json', 'w') as f:
+        json.dump(data, f, indent=2)
+
+
+for i in range(2):
+    write_json({get_key(): gen_person()})
+
 # Dz 32
-from ep.salary_employee import SalaryEmployee
-from ep.hourly_employee import HourlyEmployee
-from ep.sales_representative import SalesRepresentative
-from ep.payroll_system import PayrollSystem
-
-salary_employee = SalaryEmployee(1, "Валерий Задорожный", 1500)
-hourly_employee = HourlyEmployee(2, "Илья Кромин", 40, 15)
-sales_representative = SalesRepresentative(3, "Николай Хорольский", 1000,
-                                           250)
-
-payroll_system = PayrollSystem()
-payroll_system.calculate([
-    salary_employee,
-    hourly_employee,
-    sales_representative
-])
+# from ep.salary_employee import SalaryEmployee
+# from ep.hourly_employee import HourlyEmployee
+# from ep.sales_representative import SalesRepresentative
+# from ep.payroll_system import PayrollSystem
+#
+# salary_employee = SalaryEmployee(1, "Валерий Задорожный", 1500)
+# hourly_employee = HourlyEmployee(2, "Илья Кромин", 40, 15)
+# sales_representative = SalesRepresentative(3, "Николай Хорольский", 1000,
+#                                            250)
+#
+# payroll_system = PayrollSystem()
+# payroll_system.calculate([
+#     salary_employee,
+#     hourly_employee,
+#     sales_representative
+# ])
 
 # Dz 31
 
--- main.py
+++ main.py
@@ -7690,10 +7690,294 @@ import math
 
 
 
+# /////////////////////////////////////////////// Урок 33 //////////////////////////////////
 
 
-# # Задача
+"""Записать начало распаковки (тут рассказывают о старом)
+   pickle - работает только с python
+   Чтобы список мог работать в json мы его должны поместить в список"""
+
+import pickle
+import json
+
+# Урок 33
+
+"""Упаковка данных (сериализация, десериализация) """
+
+# file_name = 'basket.txt'
+#
+# shop_list = {
+#     'фрукты': ['яблоко', 'манго'],
+#     'овощи': ['морковь'],
+#     'Бюджет': 1000
+# }
+#
+# with open(file_name, 'wb') as fn:
+#     pickle.dump(shop_list, fn)
+#
+# with open(file_name, 'rb') as fn:
+#     shop_list_2 = pickle.load(fn)
+#
+# print(shop_list_2)
+
+
+#
+#
+# class Test:
+#     num = 35
+#     str = 'привет'
+#     lst = [1, 2, 3]
+#     tpl = (22, 23)
+#
+#     def __str__(self):
+#         return f"число: {Test.num}\nСтрока: {Test.str}\nСписок: {Test.lst}\nКортеж: {Test.tpl}"
+#
+#
+# obj = Test()
+# print(obj)
+# print()
+#
+# obj1 = pickle.dumps(obj)
+# print(f"Сериализация в строке: \n{obj1}")
+# print()
+#
+# obj2 = pickle.loads(obj1)
+# print(f"Десериализация из строки: \n{obj2}")
+
+
+# # Пропущена
 # #
+# class Tast2:
+#     def __init__(self):
+#         self.a = 35
+#         self.b = 'test'
+#         self.c = lambda x: x * x
+#
+#     def __str__(self):
+#         return f"{self.a} {self.b} {self.c}"
+#
+#     def __getstate__(self):
+#         attr = self.__dict__.copy()
+#         del attr['c']
+#         return attr
+#
+#     def __setstate__(self, state):
+#         self.__dict__ = state
+#         self.c = lambda x: x * x
+#
+#
+# item1 = Tast2()
+# print(item1)
+# item2 = pickle.dumps(item1)
+# print(item2)
+# item3 = pickle.loads(item2)
+# print(item3.__dict__)
+# print(item3)
+
+
+""" Json """
+
+# data = {
+#     'name': 'Olga',
+#     'age': 35,
+#     20: None,
+#     'hobbies': ('running', 'singing'),
+#     'children': [
+#         {
+#             'first_name': 'Alice',
+#             True: 1
+#         }
+#     ]
+# }
+#
+# file_name = 'data_file.json'
+#
+# with open(file_name, 'w', encoding='UTF-8') as fw:
+#     json.dump(data, fw)
+#
+# with open(file_name, 'r', encoding='UTF-8') as fw:
+#     data1 = json.load(fw)
+#
+# print(data1)
+
+# -------------------------------------------------------------
+
+# Сохраняем в строку.
+# Чтобы работать со строкой в json, нужно сначала ее считать.
+
+# data = {
+#     'name': 'Olga',
+#     'age': 35,
+#     '20': None,  # (1)
+#     'hobbies': ('running', 'singing'),
+#     'children': [
+#         {
+#             'first_name': 'Alice',
+#             'True': 1  # (1)
+#         }
+#     ]
+# }
+#
+# json_string = json.dumps(data, sort_keys=True)  # (1) sort_keys=True
+# # (сортировка ключей = все ключи должны быть строкой)
+# print(json_string)
+# print(type(json_string))
+#
+# data1 = json.loads(json_string)  # Считываем строку
+# print(data1)
+# print(type(data1))
+#
+#
+# # Проблемы, которые возникают при, использование другого языка
+# #
+# x = {
+#     'name': 'Виктор'
+# }
+# print(json.dumps(x))
+# print(json.loads(json.dumps(x)))  # 1 способ чтобы кодировка не билась
+# print(json.dumps(x, ensure_ascii=False))  # 2 способ чтобы кодировка не билась
+
+
+# Чтобы список мог работать в json мы его должны поместить в список
+
+# from random import choice
+#
+# def gen_person():
+#     name = ''
+#     tel = ''
+#
+#     latter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'e']
+#     num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+#
+#     while len(name) != 7:
+#         name += choice(latter)
+#     # print(name)
+#
+#     while len(tel) != 10:
+#         tel += choice(num)
+#     # print(tel)
+#
+#     person = {
+#         'name': name,
+#         'tel': tel,
+#     }
+#     return person
+
+
+# # 1 создаем список json
+# person = []  # Помещаем в список
+# for i in range(3):
+#     person.append(gen_person())
+#
+# print(person)
+#
+# with open('person.json', 'w', encoding='UTF-8') as f:
+#     json.dump(person, f, indent=2)
+
+
+# 2 с помощью try/except создаем возможность не перезаписывать, а добавлять новые данные
+# def write_json(person_dict):
+#     try:  # Помогает не перезаписывать, а прибавлять
+#         data = json.load(open('person.json'))
+#     except FileNotFoundError:
+#         data = []
+#
+#     data.append(person_dict)
+#     with open('person.json', 'w', encoding='UTF-8') as f:
+#         json.dump(data, f, indent=2)
+#
+#
+# for i in range(1):
+#     write_json(gen_person())
+
+
+
+
+# /////////////////////////////////////////////// Урок 34 //////////////////////////////////
+
+
+
+
+
+# Задача
+# возможность добавления,удаления,изменения оценок и подсчет среднего балла,добавление удаление студентов и перевод в другую группу
+# запись данных о студенте и группе студентов в файл, плюс считывание файла
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
